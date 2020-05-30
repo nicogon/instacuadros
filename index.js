68b0db64790b0e16e3206d2411b19263e6830ee5
nicogon <nicogon@gmail.com>
2020-05-30 01:22:44 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+const path = require('path');
+
+const imageDir = path.resolve(__dirname, './images/');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+  const files = fs.readdirSync(imageDir);
+  const randomFiles = shuffleArray(files);
+  res.render('index', { images: [randomFiles[0], randomFiles[1], randomFiles[2]] });
+});
+
+app.listen(3000, () => {
+  console.log('Running');
+});
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
